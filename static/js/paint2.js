f29560cb653c098f3bc48b764fa12e96656fa188
cpcdoy <cpcdoy@gmail.com>
2020-09-29 13:25:05 +0200

    Add: touch events + Fix: canvas painting when resizing window

--- static/js/paint2.js
+++ static/js/paint2.js
@@ -4,7 +4,6 @@ window.onload = function () {
     var color = '#ff0000';
     var canvas = document.getElementById("paint-canvas");
     var context = canvas.getContext("2d");
-    var boundings = canvas.getBoundingClientRect();
 
     context.fillStyle = "white";
     context.fillRect(0, 0, canvas.width, canvas.height);
@@ -74,54 +73,70 @@ window.onload = function () {
         $newColor.click();
     });
 
- 
-    /*
- 
-    // Handle Colors
-    var colors = document.getElementsByClassName('colors')[0];
- 
-    colors.addEventListener('click', function (event) {
-        context.strokeStyle = event.target.value || 'black';
-    });
- 
-    // Handle Brushes
-    var brushes = document.getElementsByClassName('brushes')[0];
- 
-    brushes.addEventListener('click', function (event) {
-        context.lineWidth = event.target.value || 1;
-    });
-    */
+    // Handle Mouse Coordinates
+    function setMouseCoordinates(event) {
+        var boundings = canvas.getBoundingClientRect();
+        mouseX = event.clientX - boundings.left;
+        mouseY = event.clientY - boundings.top;
+    }
+
+    function setTouchPos(event) {
+        var rect = canvas.getBoundingClientRect();
+        var touch = event.touches[0];
+        mouseX = touch.clientX - rect.left;
+        mouseY =  touch.clientY - rect.top;
+    }
+
+    canvas.addEventListener('touchstart', function (event) {
+        setTouchPos(event);
+        isDrawing = true;
+
+        console.log("touchstart", mouseX, mouseY);
+
+        // Start Drawing
+        context.beginPath();
+        context.moveTo(mouseX, mouseY);
+    }, false);
+
     // Mouse Down Event
     canvas.addEventListener('mousedown', function (event) {
         setMouseCoordinates(event);
         isDrawing = true;
- 
+
         // Start Drawing
         context.beginPath();
         context.moveTo(mouseX, mouseY);
     });
- 
+
     // Mouse Move Event
+    canvas.addEventListener('touchmove', function (event) {
+        setTouchPos(event);
+        console.log("touchmove", mouseX, mouseY);
+
+        if (isDrawing) {
+            context.lineTo(mouseX, mouseY);
+            context.stroke();
+        }
+    }, false);
+
     canvas.addEventListener('mousemove', function (event) {
         setMouseCoordinates(event);
- 
+
         if (isDrawing) {
             context.lineTo(mouseX, mouseY);
             context.stroke();
         }
     });
- 
+
     // Mouse Up Event
     canvas.addEventListener('mouseup', function (event) {
         setMouseCoordinates(event);
         isDrawing = false;
     });
- 
-    // Handle Mouse Coordinates
-    function setMouseCoordinates(event) {
-        mouseX = event.pageX - boundings.left;
-        mouseY = event.pageY - boundings.top;
-    }
+
+    canvas.addEventListener('touchend', function (event) {
+        isDrawing = false;
+    }, false);
 
     // Handle Clear Button
     var clearButton = document.getElementById('clear');
@@ -133,6 +148,23 @@ window.onload = function () {
         context.strokeStyle = color || defaultColor;
         context.lineWidth = lineWidth; // initial brush width
     });
+
+    // Prevent scrolling when touching the canvas
+    document.body.addEventListener("touchstart", function (e) {
+        if (e.target == canvas) {
+            e.preventDefault();
+        }
+    }, false);
+    document.body.addEventListener("touchend", function (e) {
+        if (e.target == canvas) {
+            e.preventDefault();
+        }
+    }, false);
+    document.body.addEventListener("touchmove", function (e) {
+        if (e.target == canvas) {
+            e.preventDefault();
+        }
+    }, false);
 
     // Handle Save Button
     // var saveButton = document.getElementById('save');
